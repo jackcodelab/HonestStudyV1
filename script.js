2f75f11cd41ed76275634b4fe7c54928ab14211a
Abdul Wahab <1jack.sparrow2.1687@gmail.com>
2026-08-06 21:22:33 +0500

    V1.3

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("DevTrack Loaded");
+
+const greetingElement = document.getElementById("greeting");
+
+const today = new Date();
+const currentHour = today.getHours();
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+}
+const formattedDate = today.toLocaleDateString('en-GB', options);
+
+document.getElementById('date-display').textContent = formattedDate;
+
+
+if (currentHour < 11) {
+    greetingElement.textContent = "Good morning";
+} else if (currentHour < 16) {
+    greetingElement.textContent = "Good afternoon";
+} else {
+    greetingElement.textContent = "Good evening";
+}
